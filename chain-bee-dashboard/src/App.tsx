ec61406105922235332432342a3767cc2267f61e
joselina-rz <joselina.ramirez@redb.ee>
2022-07-29 17:35:59 -0300

    dashboard seed

---
+++ chain-bee-dashboard/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App flex flex-col">
+      <div>HOLA</div>
+      <div>Jose</div>
+    </div>
+  );
+}
+
+export default App;
